286118fb8794ec8276e84a5b1de44397fe3ee95b
leon <moyanxidian@foxmail.com>
2015-08-12 02:19:53 +0800

    add LeonShopDetails.html and connect the LeonShop

--- js/plugins/jquery.plugins.js
+++ js/plugins/jquery.plugins.js
@@ -17,6 +17,95 @@
 			var $evenTr=$(this).find('tbody tr:even');
 			$evenTr.css('backgroundColor',"lightgray");
 		},
+
+		//对页面中的内容添加蒙版方法,分别可以指定图片对象,含有文本的对象,或传入一段文本
+		//其格式分别为$img.mask();$text.mask();$().mask(text);
+		//其中第三个是由于本插件作为对象方法插件而不是全局方法,所以需要添加到一个对象上来发挥作用,这里的$()指的是document对象.
+		"mask":function(text){
+			//先完成页面周围蒙版的效果,其中设置透明度为0.8,位置为固定,层次为20.
+			var $masking=$("<div id='masking' style='width: 100%;height: 100%;background-color: rgba(50,50,50,0.8);z-index: 20;position: fixed'></div>");
+
+			//通过检测对象是否含有路径属性来检测是否为图片对象
+			if($(this).attr('src')){
+				//生成图片,设置路径,添加到页面中去.
+				var $img=$("<img id='maskImg' style='height:60%;width:auto;z-index:20;position:fixed;top:10%;left:20%;cursor:pointer;' />");
+				$img.attr('src',$(this).attr('src'));
+				//				
+				$masking.bind('click',function(){
+					$masking.hide();
+					$img.hide();
+				});
+				$img.bind('mousedown',function(e){
+					var mouseOffset=e.pageX-(+($(this).css('left').slice(0,-2)));
+					var mouseTop=e.pageY-(+($(this).css('top').slice(0,-2)));
+					$img.bind('mousemove',function(event){
+						$(this).css('left',event.pageX-mouseOffset).css('top',event.pageY-mouseTop);
+						return false;
+					})					
+				}).bind('mouseup',function(event){
+					$img.unbind('mousemove');
+				});
+				$img.prependTo('body');
+				$masking.prependTo('body');
+				var oMaskImg=document.getElementById('maskImg');
+				var oMasking=document.getElementById('masking');
+				oMasking.onmousewheel=oMaskImg.onmousewheel=function(event){
+					if(event.wheelDelta>0){
+						var value=document.defaultView.getComputedStyle(oMaskImg,null).height.slice(0,-2);
+						oMaskImg.style.height=(+value+100)+"px";						
+					}else{
+						var value=document.defaultView.getComputedStyle(oMaskImg,null).height.slice(0,-2);
+						oMaskImg.style.height=(+value-100)+"px";
+					}
+					return false;
+				};
+
+
+
+			}else{
+				//当为文本对象时则设置格式,其中为了美观使用到了bootstrap中的panel
+				var $occurWindow=$("<div class='panel panel-primary container' style='position: fixed;top: 20%;left: 20%;width:60%;right: auto;height: 40%;z-index:20;'></div>");
+				var $panelHead=$("<div class='panel-heading'>感谢您的购买.</div>");
+				var $panelBody=$("<div class='panel-body' style='padding: 20px;height: 70%;'></div>");
+				var $panelFoot=$("<div class='panel-footer'></div>");
+				var $closeBtn=$("<button class='btn btn-default'>关闭</button>");
+				$closeBtn.appendTo($panelFoot);
+				$closeBtn.bind('click',function(){
+					$masking.hide();
+					$occurWindow.hide();
+				});
+				//检测当为文本对象时,则设置其内容为蒙版内容,否则将字符串传递给蒙版文本.
+				if(text){
+					$panelBody.text(text);
+				}else{
+					$panelBody.text($(this).text());
+				}				
+				$panelHead.appendTo($occurWindow);
+				$panelBody.appendTo($occurWindow);
+				$panelFoot.appendTo($occurWindow);
+				$occurWindow.appendTo('body');
+				$masking.prependTo('body');	
+			}					
+		},
+
+		"imgReal": function(){
+			//$(this).attr('data-realWidth','value');
+			var tempImg=new Image();
+			var value=0;
+			var that=this;
+			tempImg.src=$(this).attr('src');
+			
+			if(tempImg.complete){				
+				value=tempImg.width;
+			}			
+			tempImg.onload=function(){
+				var value=10;
+				value=tempImg.width;	
+				return $(this);
+				return false;	
+			}			
+			
+		}
 	});
 	
 	//扩展全局选择器
