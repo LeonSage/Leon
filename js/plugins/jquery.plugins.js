952eef3862d1ac6c74658a1c1b556a773d9099cc
leon <moyanxidian@foxmail.com>
2015-08-12 22:25:11 +0800

    add describe infomation and optimizate the mask() plugins.

--- js/plugins/jquery.plugins.js
+++ js/plugins/jquery.plugins.js
@@ -5,7 +5,6 @@
 	$.fn.extend({
 		"color":function(value){
 			if(value==undefined){
-
 				//return本对象以便插件的链式操作.
 				return this.css('color');				
 			}else{
@@ -18,16 +17,14 @@
 			//定义一个图片并将路径添加后加载
 			var tempImg=new Image();
 			tempImg.src=$(this).attr('src');
-			//如果图片加载完成则直接设置
+			//如果图片加载完成则使用回调函数传回对应的宽度和高度
 			if(tempImg.complete){
 				callback(tempImg.width,tempImg.height);	
-			}							
+			}
+			//如果图片未加载则在加载时使用回调函数(回调函数是必须的,因为这里是异步加载,不能保证调用时获取到的正确值,所以要在执行后返回,使用回调).						
 			tempImg.onload=function(){
-				//这里给对象添加一个存储实际尺寸的属性
 				callback(tempImg.width,tempImg.height);
-
-			}		
-			
+			}			
 		},
 		"striped":function(){
 			$(this).find('thead').css('backgroundColor','lightblue');
@@ -41,32 +38,42 @@
 		"mask":function(text){
 			//先完成页面周围蒙版的效果,其中设置透明度为0.8,位置为固定,层次为20.
 			var $masking=$("<div id='masking' style='width: 100%;height: 100%;background-color: rgba(50,50,50,0.8);z-index: 20;position: fixed'></div>");
-
 			//通过检测对象是否含有路径属性来检测是否为图片对象
 			if($(this).attr('src')){
 				//生成图片,设置路径,添加到页面中去.
 				var $img=$("<img id='maskImg' style='height:60%;width:auto;z-index:20;position:fixed;top:10%;left:20%;cursor:pointer;' />");
 				$img.attr('src',$(this).attr('src'));
-				//				
+				//当点击外侧蒙版时,移除元素(如果只是隐藏的话会带来内存的额外消耗,所以应当使用remove()方法).			
 				$masking.bind('click',function(){
-					$masking.hide();
-					$img.hide();
+					$masking.remove();
+					$img.remove();
 				});
+
+				//当在图片上按下鼠标左键时可以拖动图片移动
 				$img.bind('mousedown',function(e){
+					//使用slice方法获取不含有px的数字
 					var mouseOffset=e.pageX-(+($(this).css('left').slice(0,-2)));
 					var mouseTop=e.pageY-(+($(this).css('top').slice(0,-2)));
+					//当鼠标按下且移动时拖动图片
 					$img.bind('mousemove',function(event){
 						$(this).css('left',event.pageX-mouseOffset).css('top',event.pageY-mouseTop);
+						//并取消冒泡和默认事件
 						return false;
 					})					
 				}).bind('mouseup',function(event){
+					//当鼠标松开时,取消绑定的鼠标移动事件.
 					$img.unbind('mousemove');
 				});
+				//将图片和蒙版添加到页面中去.
 				$img.prependTo('body');
 				$masking.prependTo('body');
-				var oMaskImg=document.getElementById('maskImg');
+				
+				//鼠标滚轮控制图片大小的功能(jQuery使用鼠标滚动需要一个插件,太麻烦,直接用js自己写一个).
+				//当鼠标在蒙版或图片上滚动时均可控制图片大小.
 				var oMasking=document.getElementById('masking');
+				var oMaskImg=document.getElementById('maskImg');
 				oMasking.onmousewheel=oMaskImg.onmousewheel=function(event){
+					//如果鼠标向上滚动(wheelDelta是120的倍数,如果为正,则向上,为负则向下滚动).
 					if(event.wheelDelta>0){
 						var value=document.defaultView.getComputedStyle(oMaskImg,null).height.slice(0,-2);
 						oMaskImg.style.height=(+value+100)+"px";						
@@ -74,10 +81,9 @@
 						var value=document.defaultView.getComputedStyle(oMaskImg,null).height.slice(0,-2);
 						oMaskImg.style.height=(+value-100)+"px";
 					}
+					//取消冒泡和默认行为(这里直接设置鼠标在蒙版上滚动时后面页面上的内容禁止滚动).
 					return false;
 				};
-
-
 
 			}else{
 				//当为文本对象时则设置格式,其中为了美观使用到了bootstrap中的panel
@@ -88,8 +94,8 @@
 				var $closeBtn=$("<button class='btn btn-default'>关闭</button>");
 				$closeBtn.appendTo($panelFoot);
 				$closeBtn.bind('click',function(){
-					$masking.hide();
-					$occurWindow.hide();
+					$masking.remove();
+					$occurWindow.remove();
 				});
 				//检测当为文本对象时,则设置其内容为蒙版内容,否则将字符串传递给蒙版文本.
 				if(text){
